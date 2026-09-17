98b87935b5b2eca0b13ffc0d3ead79e673df3cca
g.albieri <126593217+GCRA101@users.noreply.github.com>
2026-09-17 15:02:05 +0100

    Update deprecated pushButtonData.LongDescription
    Replace deprecated pushButtonData.LongDescription (valid for versions of Revit earlier than 2023) with pushButton.LongDescription (applies to Revit 2023-2026).

--- ViewFiltersFactory/RibbonItemFactory.cs
+++ ViewFiltersFactory/RibbonItemFactory.cs
@@ -70,7 +70,7 @@ namespace ViewFiltersFactory
             pushButton.LargeImage = BitmapImageFactory.getInstance().create(largeImagePath);
             pushButton.ToolTipImage = BitmapImageFactory.getInstance().create(toolTipImagePath);
             pushButton.ToolTip = toolTipText;
-            pushButtonData.LongDescription = longDescription;
+            pushButton.LongDescription = longDescription;
 
             return pushButton;
 
